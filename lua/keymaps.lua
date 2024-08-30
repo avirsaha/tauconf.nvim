ef929891a9d6b81a6ad299358d6bd738a21426f8
Aviraj Saha <aviraj.saha@outlook.con>
2024-08-30 09:20:59 +0530

    Added keymaps to adjust window splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,19 +9,22 @@ local opts = { noremap = true, silent = true }
 -- vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
 -- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, opts)
-vim.keymap.set('n', '<C-Up>', 'dkkp', opts)
-vim.keymap.set('n', '<C-Down>', 'djjp', opts)
+vim.keymap.set('n', '<C-Up>', 'ddkkp', opts)
+vim.keymap.set('n', '<C-Down>', 'ddp', opts)
 vim.keymap.set('n', '<leader>-', '^i-- <Esc>', opts)
 vim.keymap.set('n', '<leader>/', '^i// <Esc>', opts)
 vim.keymap.set('n', '<leader>#', '^i# <Esc>', opts)
 vim.keymap.set('n', '<leader>w', vim.cmd.w, opts)
--- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, opts)
--- vim.keymap.set('n', '<C-Up>', 'dkkp', opts)
--- vim.keymap.set('n', '<C-Down>', 'djjp', opts)
+-- Increase/Decrease horizontal split height
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', opts)       -- Increase height
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', opts)     -- Decrease height
 
--- Insert mode mappings
--- vim.keymap.set('i', 'jk', '<Esc>', opts)
--- map('i', '<leader>em', '<Esc>', opts)
+-- Increase/Decrease vertical split width
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)  -- Increase width
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)   -- Decrease width
+--Insert mode mappings
+--vim.keymap.set('i', 'jk', '<Esc>', opts)
+--map('i', '<leader>em', '<Esc>', opts)
 
 -- Visual mode mappings
 -- vim.keymap.set('v', '<', '<gv', opts)
