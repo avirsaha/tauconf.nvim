7152decb653b3e1cbbffd7e75e253cc61fbf1238
Aviraj Saha <aviraj.saha@outlook.con>
2024-09-12 13:57:21 +0530

    fix - change color scheme and remove catppuccin

---
+++ lua/plugins/mountaineer.lua
@@ -0,0 +1,10 @@
+return {
+  'TheNiteCoder/mountaineer.vim',
+  name = 'mountaineer', 
+  priority = 1000,
+  config = function()
+	vim.cmd 'set background=dark'
+	vim.cmd 'colorscheme mountaineer'
+
+end
+}
