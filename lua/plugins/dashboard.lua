5801c2e01e18d3197698bc6e35aec372a1ebf0a3
Aviraj Saha <aviraj.saha@outlook.con>
2024-09-18 23:16:02 +0530

    feat - add basic dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -9,18 +9,34 @@ return{
 	--shortcut_type   --  shorcut type 'letter' or 'number'
 	shuffle_letter = true,  --  default is true, shortcut 'letter' will be randomize, set to false to have ordered letter.
 	change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
-	config = {},    --  config used for theme
-	hide = {
-	  statusline = true    -- hide statusline default is true
-	  --tabline       -- hide the tabline
-	  --winbar        -- hide winbar
-	},
-	preview = {
-	  --command       -- preview command
-	  --file_path     -- preview file path
-	  --file_height   -- preview file height
-	  --file_width    -- preview file width
-	},
-    }
+	config = {
+    header = {
+[[ _                               __            _           ]], 
+[[| |                             / _|          (_)          ]],
+[[| |_ __ _ _   _  ___ ___  _ __ | |_ _ ____   ___ _ __ ___  ]], 
+[[| __/ _` | | | |/ __/ _ \| '_ \|  _| '_ \ \ / / | '_ ` _ \ ]],
+[[| || (_| | |_| | (_| (_) | | | | |_| | | \ V /| | | | | | |]],
+[[ \__\__,_|\__,_|\___\___/|_| |_|_(_)_| |_|\_/ |_|_| |_| |_|]],
+[[]],
+    },
+
+shortcut = {
+    -- action can be a function type
+    { desc = 'Highly customizable neovim config'},
+  },
+  packages = { enable = true }, -- show how many plugins neovim loaded
+  -- limit how many projects list, action when you press key or enter it will run this action.
+  -- action can be a functino type, e.g.
+  -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
+  project = { enable = false, limit = 8, icon = 'your icon', label = '', action = 'Telescope find_files cwd=' },
+  mru = {enable = false, limit = 10, icon = 'your icon', label = '', cwd_only = false },
+  footer = {[[]],
+  [[]],
+  [[]],
+    [[The best tools are the ones that bend to your will,]],
+  [[not the ones that force you to change your ways.]], 
+  [[                                                - a random tau]],}
+}
+}
   end,
 }
