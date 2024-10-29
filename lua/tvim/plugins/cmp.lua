9018a7e7a5874de7cc89cc3a4782894313321481
Aviraj Saha <aviraj.saha@outlook.com>
2024-10-30 01:13:23 +0530

    refactor: Code formatting.

--- lua/tvim/plugins/cmp.lua
+++ lua/tvim/plugins/cmp.lua
@@ -18,7 +18,7 @@ return {
     cmp.setup({
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)         -- For `LuaSnip` users
+          luasnip.lsp_expand(args.body) -- For `LuaSnip` users
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -26,7 +26,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),       -- Accept currently selected item
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
