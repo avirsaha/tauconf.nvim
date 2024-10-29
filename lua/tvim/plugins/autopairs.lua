f15cb470206a0accad6d2d97795c417b84670530
Aviraj Saha <aviraj.saha@outlook.com>
2024-10-29 23:12:55 +0530

    fix: Update the autopairs.lua file to fix syntax error.

--- lua/tvim/plugins/autopairs.lua
+++ lua/tvim/plugins/autopairs.lua
@@ -2,23 +2,22 @@ return {
   'avirsaha/nvim-autopairs',
   event = 'InsertEnter',
   config = function()
-    require('nvim-autopairs').setup {
+    local cmp = require("cmp")
+    local npairs = require("nvim-autopairs")
+    npairs.setup {
       disable_filetype = { 'TelescopePrompt', 'vim' },
-  fast_wrap = {
-    map = '<M-e>', -- The key used to activate fast wrap (e.g., Alt+E)
-
-    chars = { '{', '[', '(', '"', "'" }, -- The characters used for wrapping
-      pattern = [=[%.'%['%{%"%s%}]]=], -- Pattern to identify where fast wrapping can occur
-      offset = 0, -- Offset for cursor position after wrapping
-      end_key = 'l', -- The key used to complete the fast wrap
-      keys = 'qwertyuiopzxcvbnmasdfghjkl', -- Keys for quick selection of wrap characters
-      check_comma = true, -- Check for commas in the line
-      highlight = 'Search', -- Highlight the selected wrap character
-    },
-
-  local cmp = require('cmp'),
-  cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
-
+      fast_wrap = {
+        map = '<M-e>',                       -- The key used to activate fast wrap (e.g., Alt+E)
+        chars = { '{', '[', '(', '"', "'" }, -- The characters used for wrapping
+        pattern = [=[%.'%['%{%"%s%}]]=],     -- Pattern to identify where fast wrapping can occur
+        offset = 0,                          -- Offset for cursor position after wrapping
+        end_key = 'l',                       -- The key used to complete the fast wrap
+        keys = 'qwertyuiopzxcvbnmasdfghjkl', -- Keys for quick selection of wrap characters
+        check_comma = true,                  -- Check for commas in the line
+        highlight = 'Search',                -- Highlight the selected wrap character
+      },
     }
+    -- Setup integration with nvim-cmp
+    cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
   end,
 }
