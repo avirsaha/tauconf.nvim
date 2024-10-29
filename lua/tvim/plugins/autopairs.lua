6226798ba562b02def70f72500135604adcb6b71
Aviraj Saha <aviraj.saha@outlook.com>
2024-10-29 16:23:57 +0530

    feat: Configure nvim-autopairs to integrate with nvim-cmp and enable fast_wrap
    - Integrate nvim-autopairs with nvim-cmp for automatic closing of pairs during completion.
    - Enable fast_wrap feature for quick wrapping of selections with specified delimiters.
    - Update key mappings for seamless navigation and insertion of pairs.

--- lua/tvim/plugins/autopairs.lua
+++ lua/tvim/plugins/autopairs.lua
@@ -4,6 +4,21 @@ return {
   config = function()
     require('nvim-autopairs').setup {
       disable_filetype = { 'TelescopePrompt', 'vim' },
+  fast_wrap = {
+    map = '<M-e>', -- The key used to activate fast wrap (e.g., Alt+E)
+
+    chars = { '{', '[', '(', '"', "'" }, -- The characters used for wrapping
+      pattern = [=[%.'%['%{%"%s%}]]=], -- Pattern to identify where fast wrapping can occur
+      offset = 0, -- Offset for cursor position after wrapping
+      end_key = 'l', -- The key used to complete the fast wrap
+      keys = 'qwertyuiopzxcvbnmasdfghjkl', -- Keys for quick selection of wrap characters
+      check_comma = true, -- Check for commas in the line
+      highlight = 'Search', -- Highlight the selected wrap character
+    },
+
+  local cmp = require('cmp'),
+  cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
+
     }
   end,
 }
